51d54ff125fcfbca5c582cbc340f0376918bff99
Brala2002 <brallangombel@gmail.com>
2023-05-13 15:06:47 -0500

    arreglo de estilos (tarjeta de Kevin)

--- src/components/CardCompra/Card-view.jsx
+++ src/components/CardCompra/Card-view.jsx
@@ -34,30 +34,29 @@ export const CardView = () => {
                 </div>
 
             
+
+        </div >
             <div className={`products-preview ${previewActive ? 'active' : ''}`}>
 
                 <div className="preview" data-target="p-1">
-                <i className="fas fa-times" onClick={closePreview}></i>
+                <i className="fas fa-times" onClick={closePreview} id="cierre-ventana"></i>
                     <img className='cardBig' src={imgcard} alt="" />
                    
                     <h2>Evento</h2>
                     <div className="info">
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab minus quae animi consequuntur eaque sed quisquam libero, doloremque numquam? Mollitia, obcaecati in hic vero molestiae nihil praesentium laborum autem inventore.</p>
                     
-                        <div className="date"><i className='bx bx-calendar'></i>Fecha: 02/08/24</div>
-                        <div className="place"><i className='bx bx-map' ></i>Lugar: venecia</div>
-                        <div className="time"><i className='bx bx-time'></i>Hora: 2:40</div>
-                        <div className="price"><i className='bx bx-purchase-tag-alt' ></i>  Precio: $2.00</div>
+                        <div className="date"><i className='bx bx-calendar'></i><b>Fecha:</b> 02/08/24</div>
+                        <div className="place"><i className='bx bx-map' ></i><b>Lugar:</b> venecia</div>
+                        <div className="time"><i className='bx bx-time'></i><b>Hora:</b> 2:40</div>
+                        <div className="price"><i className='bx bx-purchase-tag-alt' ></i> <b>Precio:</b> $2.00</div>
                     </div>
                     <div className="buttons">
-
-                        <a href="#" className="reserv">Reserva ya!</a>
-                        <a href="#" className="buy">Compra ahora!</a>
+                        <button className='reserv'>!Reserva ya!</button>
+                        <button className='buy'>!Compra ahora!</button>
                     </div>
                 </div>
             </div>
-
-        </div >
         </>
      )
 }
