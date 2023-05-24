dda019c1b26e9a20a216800cce852bd8c041bfde
Yeison2129 <yeisondavidcastiblanco11@gmail.com>
2023-05-24 13:57:23 -0500

    ADD: logica del mapa en el crud de eventos

--- src/components/Mapa/MapView.jsx
+++ src/components/Mapa/MapView.jsx
@@ -4,20 +4,26 @@ import data from "./assets/data.json";
 import Markers from "./VenueMarkers";
 
 import { useLocation, useNavigate } from "react-router-dom";
-    
+
 import "leaflet/dist/leaflet.css";
 
 const MapView = (props) => {
   const [state, setState] = useState({
-    currentLocation: { lat: 4.45, lng: -75.48700 },
+    currentLocation: { lat: 4.45, lng: -75.487 },
     zoom: 10,
     data,
   });
 
-
-
   return (
-    <MapContainer className="mapView" center={state.currentLocation} zoom={state.zoom} dragging={false} scrollWheelZoom={false}>
+    <MapContainer
+      className="mapView"
+      center={state.currentLocation}
+      zoom={state.zoom}
+      dragging={false}
+      scrollWheelZoom={false}
+      minZoom={11}
+      doubleClickZoom={false}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
