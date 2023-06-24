3e2f22d0de0dbad7896ccdafb861ae0a58d8ebbd
Your Name <you@example.com>
2023-06-24 14:32:16 -0500

    update: se agrega icon a componente asistencias, sigue en desarrollo

--- src/components/Card-Asist/Card_Asist.jsx
+++ src/components/Card-Asist/Card_Asist.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import "./cardasist.css";
 import { useState } from "react";
 import swal from "sweetalert2";
-import {asistEvents,consultAsist,getEvents,getEventsCompany,AsistEventCompany,} from "../../api/App";
+import { asistEvents, consultAsist, getEvents, getEventsCompany, AsistEventCompany, } from "../../api/App";
 import { Formik, Field } from "formik";
 import { Dashboard } from "../dashboard_empresa/Dashboard_Empresa";
 
@@ -89,22 +89,17 @@ export const CardAsist = () => {
                       backgroundImage: `url(${eventEmpresa.img_event})`,
                     }}
                   ></div>
-                      <div className="head-card">
-              <div className="icon-card">
-                <div className="button-delete">
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAPNJREFUWEftmOERwiAMRl9HcQIdxREcQSdQN3AUN3GFuomH195xlJIE2rvqhZ9tCF9fAiTt2NjoNqaHnxd0BK7AQUm2By7AU2lvIhREvLSOE7sdEMSJwxKy20BHdJoxCJQemom1gu5AEFga8Qdo7L++XJAUthyhllyR1kvfT0LpghJEKkLxnKqdkomb2o+0y9SOljoC/kbQHDnr8wnYWkLWhdWhd0FA8V5zQk4oKuLiXPFdNlaP1vPJD0aJ3GKErJXh6kntgkYCaxT8xaZRusta2ue5sBbbaklQcHoCzsDemjiJ/XvwU/zxoBHUqMM23QVJvD4tFJglZPGVZgAAAABJRU5ErkJggg==" onClick={() => {
-                    pregunta(eventEmpresa.id_event);
-                  }}key={eventEmpresa.img_event}/>
-                </div>
-              </div>
-              <div className="icon-card">
-                <img
-                  id="img-card-pointer"
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAaFJREFUWEftl4FNxDAMRf9NABvACLABTAAbABvAJLABYgJgAtgARoANuA3QlxzkC07ye01OlaBSpUp14xf7J/ldYWHXamE8+LNA+wAurBsPAL5KndlFhY4APAI4NAjCnAJ4j6BGAxHmBQAr5K8i1EigHGZtRHu1So0CimBODOQVQIL6AHDsNTUCqASTNMP3HuoGwF3qZ2+gSDO5XvKYYUAlAXPyCYrPXuSfAPjdzzbQq0JRm66tFV7EBEorjiKnrjaWfw+gmmZyvSSphDB8OReoJWDmuARw7zahIsxcIAVGidnYMbetkJJIifl1emwDpCRSYsLzdSqQkkiJKdqwKUBKIiWm6glVICWREtM0qAqQkkiJacIoy15JpMRIMC0gJZESI8O0gOhVDmy0aHftDlMDYrK3XcPUgHhS3xrQM4BzV/chlWkZtCcAZxbkDdRQmFqFaJiSj7myZ3oX3lU/M0nBQXC0D3n9lMavWog5UBGQ10809jCYUstyIALwLyHd4R/nnKr4b0tHB10etUKIoQD5RJSzrNfkpXH+gVplWlyFvgFdfY8lhhs2YQAAAABJRU5ErkJggg=="
-                  onClick={() => openForm(eventEmpresa.id_event)}
-                />
-              </div>
-            </div>
+                  <div className="head-card">
+                    <div className="icon-card">
+
+                    </div>
+                    <div className="icon-card">
+
+
+                      <i id="img-card-pointer" className="fa fa-solid fa-users" onClick={() => openForm(eventEmpresa.id_event)} />
+
+                    </div>
+                  </div>
                   <div className="imagen-card">
                     <img
                       src={eventEmpresa.img_event}
