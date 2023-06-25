52939acba188c57a80cd7f36bc09432a748bc837
ADSI-CELADA <santiagocelada13@gmail.com>
2023-06-25 13:30:54 -0500

    Les arreglo todos sus problemas :<

--- src/components/DropDown/Drop2/Drop2.jsx
+++ src/components/DropDown/Drop2/Drop2.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTable } from 'react-table';
 import './drop.css';
 import { AsistEventCompany } from '../../../api/App';
 
-export const Drop2 = () => {
+export const Drop2 = ({ id }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [user, setUser]= useState([])
-  
+  const [user, setUser] = useState([])
+
+  const x = async (id_) => {
+    let respoe = await AsistEventCompany(id_)
+    setUser(respoe.data.data)
+  };
+
+  useEffect(() => {
+    x(id)
+  }, [])
+
+
+
+
 
   return (
     <div className="dropdown">
-      <i id="img-card-pointer" className="fa fa-solid fa-users "  onClick={() => setIsOpen(!isOpen)} />
+      <i id="img-card-pointer" className="fa fa-solid fa-users " onClick={() => setIsOpen(!isOpen)} />
 
       {isOpen && (
         <div className="dropdown-content">
-         
 
-<h1>Persona que asistiran a tu evento</h1>
-<table class="rwd-table">
-  <tr>
-    <th>Nombre</th>
-    <th>No. Documento</th>
-    <th>Teléfono</th>
-    <th >Correo</th>
-  </tr>
-  <tr >
-    <td data-th="Nombre">Mariana Valencia Henao</td>
-    <td data-th="No. Documento">1095178011</td>
-    <td data-th="Telefono">3232930273</td>
-    <td id='correo-style' data-th="Correo">marianavalenciahenao0@gmail.com</td>
-  </tr>
-</table>
+
+          <h1>Persona que asistiran a tu evento</h1>
+          {
+            user.map((x) => (
+              <table class="rwd-table">
+            <tr>
+              <th>Nombre</th>
+              <th>No. Documento</th>
+              <th>Teléfono</th>
+              <th >Correo</th>
+            </tr>
+            <tr >
+              <td data-th="Nombre"> {x.nom_user} </td>
+              <td data-th="No. Documento"> {x.document_user} </td>
+              <td data-th="Telefono"> {x.phone_user} </td>
+              <td id='correo-style' data-th="Correo"> {x.mail_user} </td>
+            </tr>
+          </table>
+            ))
+          }
 
         </div>
       )}
